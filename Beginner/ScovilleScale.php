ef29355f20692885aefa71247d369e210d4b5d68
[Julie Lechartier] <bourg.juju@gmail.com>
2024-11-04 14:48:09 +0100

    [new] - Scoville Scale calcul

---
+++ Beginner/ScovilleScale.php
@@ -0,0 +1,29 @@
+<?php
+//On a une list d'ingrédient avec à chaque fois le nom du piment, la
+//quantité incorporée en gramme et son intensité sur l'échelle de Scoville. Les 3 éléments sont séparés par « : ».
+$piments = [
+    'poivrons:20:0', // 0
+    'piment-komodo-dragon:5:1800000',//1800000
+    'piment-madagascar:10:125000',//125000
+    'piment-bhut-jolokia:10:950000'//950000
+];
+$totalGram = 700;
+// on sépare les noms des quantités pour pouvoir faire les calculs
+foreach ($piments as $piment) {
+        $extract = explode(":", $piment);
+        $name = $extract[0];
+        $quantity = $extract[1];
+        $pimentScoville = $extract[2];
+        $pimentGram[] = [
+            'name' => $name,
+            'quantity' => (int)$quantity,
+            'pimentScoville' => $pimentScoville
+        ];
+}
+print_r($pimentGram);
+// Calcul
+// pimentScovilleEchelle * pimentGram / totalGram = resultat
+foreach ($pimentGram as $piment) {
+    $result = $piment['pimentScoville'] * $piment['quantity'] / $totalGram;
+}
+echo ceil($result);
