1fd229e7f8060433e61c60a88a24e3f24ebb6556
[Julie Lechartier] <bourg.juju@gmail.com>
2024-11-07 09:15:06 +0100

    [new] - algo Grinch end

--- Beginner/Grinch.php
+++ Beginner/Grinch.php
@@ -1,8 +1,8 @@
 <?php
 //Le grnich effrai des enfant, le enfant on chacun une degrès de peur
-$kids = ['Lea_1', 'Eve_10', 'Pauline_10', 'Mia_7', 'Tiago_1', 'Beatrice_3', 'Kevin_1'];
-$fear = 4;
-$time = 44;
+$kids = ['Henri_7', 'Catherine_10', 'Gabriel_1', 'Elena_6', 'Juliette_6', 'Arthur_9', 'Anna_7'];
+$fear = 5;
+$time = 59;
 
 // take information in the kids array
 foreach ($kids as $kid){
@@ -16,9 +16,12 @@ print_r($childs);
 for ($i = 0; $i < count($kids); $i++) {
     if($fear > $childs[$i][1]) {
         $time -= 3;
+        $childAfraid[] = substr($childs[$i][0], 0, 1);
     }
     elseif ($fear < $childs[$i][1]){
         $time -= 5;
     }
 }
 echo $time;
+print_r($childAfraid);
+echo 'Les enfants effrayé sont ' . $childAfraid[0];
